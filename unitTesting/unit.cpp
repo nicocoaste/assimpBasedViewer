7c60839c191196095f34641f5df771c5349001f7
nicocoaste <github.np@gmail.com>
2011-08-25 00:13:15 +0900

    Now the unit test works with 2 threads and a moving cube is displayed.

--- unitTesting/unit.cpp
+++ unitTesting/unit.cpp
@@ -20,8 +20,16 @@
 */
 
 #include "assimpBasedViewer/assimpBasedViewer.h"
+#include <sys/time.h>
+#include <pthread.h>
 
 CassimpBasedViewer *global_pointer;
+
+int64_t ts_now (void) {
+  struct timeval tv;
+  gettimeofday (&tv,NULL);
+  return (int64_t) tv.tv_sec * 1000000 + tv.tv_usec;
+}
 
 void display(void) {
     if (global_pointer) {
@@ -47,23 +55,64 @@ void MotionCB(int _x, int _y) {
     }        
 }
 
-int main (int argc, char *argv[]) {
+void loop(void) {
+    if (global_pointer) {
+	global_pointer->loop();
+    }    
+}
+
+void *thread_1(void *ptr) {
     
-    CassimpBasedViewer aBViewer;
-    global_pointer = &aBViewer;
-    
-    glutCreateWindow("assimpBasedViewer");
-    
+    glutCreateWindow("assimpBasedViewer");    
     glutDisplayFunc(display);
     glutReshapeFunc(reshape);
     glutMouseFunc(MouseCB);
     glutMotionFunc(MotionCB);
+    
+    if (global_pointer) {
+	global_pointer->loop();
+    }    
+    
+    return NULL;
+}
+
+void *thread_2(void *ptr) {
+    
+    const struct aiScene* ai_scene2 = aiImportFile("../../unitTesting/cube.3ds",aiProcessPreset_TargetRealtime_Quality);
+    
+    if (global_pointer) {
+	global_pointer->addObject(ai_scene2, "cube2");
+    }
+    
+    int64_t time_start = ts_now();
+    
+    while(1) {
+	aiVector3D vshift( 10.0* sin( ((double) (ts_now() - time_start)) / 1000000.0 ), 0.0, 0.0 );
+	aiMatrix4x4 matr;
+	aiMatrix4x4::Translation(vshift, matr);     
+	ai_scene2->mRootNode->mTransformation = matr;
+    }
+    
+    return NULL;
+}
+
+int main (int argc, char *argv[]) {
+    
+    CassimpBasedViewer aBViewer;
+    global_pointer = &aBViewer;
 
     const struct aiScene* ai_scene = aiImportFile("../../unitTesting/cube.3ds",aiProcessPreset_TargetRealtime_Quality);
     
     aBViewer.addObject(ai_scene, "cube");
     
-    aBViewer.loop();
+    pthread_t thread1, thread2;
+    const char *message1 = "Thread 1";
+    const char *message2 = "Thread 2";
+    
+    pthread_create( &thread1, NULL, thread_1, (void*) message1);
+    pthread_create( &thread2, NULL, thread_2, (void*) message2);
+    pthread_join( thread1, NULL);
+    pthread_join( thread2, NULL); 
     
     return 0;
 
